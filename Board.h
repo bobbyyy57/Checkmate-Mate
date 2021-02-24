6ef8a0ef115d3feae9f4e3259c8379e44d68df7b
bobbyyy57 <bobbylerias@yahoo.com>
2021-02-23 23:08:11 -0800

    Completed Quit and Play Button. Implemented Beginning and Game Menu. Made Greeting and Closing Messages. Made checkerboard.

---
+++ Board.h
@@ -0,0 +1,14 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include "button.hpp"
+#include "Menu.h"
+
+using namespace std;
+
+class Board {
+        public:
+		void printBoard();
+};
+
+#endif
---
+++ operations.hpp
@@ -0,0 +1,16 @@
+#ifndef BUTTON_HPP
+#define BUTTON_HPP
+
+using namespace std;
+
+#include "Menu.h"
+
+class Operations {
+        public:
+		
+	//Quit Operations
+	
+	
+};
+
+#endif
