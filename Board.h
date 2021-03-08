e69a42a29e44973af470e2d26774a3a1caa2e4b7
ashley-bautista <abaut047@hammer.cs.ucr.edu>
2021-03-07 18:22:15 -0800

    create initial and throwaway board

--- Board.h
+++ Board.h
@@ -12,10 +12,30 @@ struct Positions{
     public:
 	char SetRow(string x) { row = x; }
 	Piece GetPiece() { return type; };
-	`string GetColor() { return color; };
+	string GetColor() { return color; };
         void SetColumn(int col){ column = col; } 
         char GetRow(){return row;}
 }
+
+class Board{
+   protected:
+	const char initialBoard[8][8] = {
+       		{ 'R',  'N',  'B',  'Q',  'K',  'B',  'N',  'R' },
+      		{ 'P',  'P',  'P',  'P',  'P',  'P',  'P',  'P' },
+      		{ 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 },
+      		{ 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 },
+      		{ 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 },
+      		{ 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 },
+      		{ 'p',  'p',  'p',  'p',  'p',  'p',  'p',  'p' },
+      		{ 'r',  'n',  'b',  'q',  'k',  'b',  'n',  'r' },
+	};
+	char throwawayBoard[8][8];
+   public:
+	void printSituation
+};
+
+
+
 
 
 #endif
