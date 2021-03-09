c9c1bcd93d33c18afab0bdf02fbad8d74dcfd527
ashley-bautista <abaut047@hammer.cs.ucr.edu>
2021-03-08 20:54:25 -0800

    make separate files for surrender and move

---
+++ surrender.hpp
@@ -0,0 +1,27 @@
+#ifndef __SURRENDER_HPP__
+#define __SURRENDER_HPP__
+#include "button.hpp"
+#include "Menu.h"
+
+
+
+class Surrender : public Button{
+	public:
+		void surrender(){
+        		//called from main when user chooses S or s
+        		char choice;
+        		cout << "Are you sure you would like to surrender? (Y)ES/(N)O: " << endl;
+        		cin >> choice;
+       			if(toupper(choice) == 'Y'){
+                		cout << "You chose to surrender" << endl;
+                		if(currentTurn == 1){
+                        		cout << "Player 2 wins!!" << endl;
+               			}
+                		else{cout << "Player 1 wins!!" << endl;}
+                		quit();
+       		 	}
+        		else{
+               		 //printBoard(); (still need to implement)
+               		printGameMenu();
+        }
+}
