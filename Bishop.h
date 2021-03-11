a5984e074ea632af79475f6eb35173c51001bc5f
lb-rodrigo <77036781+lb-rodrigo@users.noreply.github.com>
2021-03-10 16:41:14 -0800

    Update Bishop.h

--- Bishop.h
+++ Bishop.h
@@ -12,51 +12,48 @@ class Bishop: public Piece{
 	
 	public:
 	 virtual string getType() { return "Bishop"; }
-
-
-	virtual string getType() { return "Bishop"; }
         virtual bool isValid(Position* start, Position* end, Board b) {
-	
-/*
+
+
                 if(start->GetColor() != b.getTurn()){
-		std::cout << "ERROR: Wrong color!" << std::endl;
-			return false;
-		}
+                std::cout << "ERROR: Wrong color!" << std::endl;
+                        return false;
+                }
 
-		if(end->GetColor() == start->GetColor()){
-		std::cout <<"ERROR: You cant eat your own, have decency and only eat the oponent!" << std::endl;
-			return false;
-		}
+                if(start->GetColor() == end->GetColor()){
+                std::cout <<"ERROR: You cant eat your own, have decency and only eat the oponent!" << std::endl;
+                        return false;
+                }
 
-		if((start->GetRow() == end->GetRow()) && (end->GetColumn() == start->GetColumn())){
-		std::cout <<"ERROR: You can stay in the same position!"<<std::endl;
-		}
+                if((start->GetRow() == end->GetRow()) && (end->GetColumn() == start->GetColumn())){
+                std::cout <<"ERROR: You can stay in the same position!"<<std::endl;
+                }
 
-		bool flag = false;
-		int bishopR = start->GetRow();
-		int bishopC = start->GetColumn();
-		int finishR = end->GetRow();
-		int finishC = end->GetColumn();
+                bool flag = false;
+                int bishopR = start->GetRow();
+                int bishopC = start->GetColumn();
+                int finishR = end->GetRow();
+                int finishC = end->GetColumn();
 
-		if(abs(bishopR - finishR) == abs(bishopC - finishC)){
-			int increment_r = (finishR - bishopR) / (abs(finishR - bishopR));
-			int increment_c = (finishC - bishopC) / (abs(finishC - bishopC));
+                if(abs(bishopR - finishR) == abs(bishopC - finishC)){
+                        int increment_r = (finishR - bishopR) / (abs(finishR - bishopR));
+                        int increment_c = (finishC - bishopC) / (abs(finishC - bishopC));
 //FOR LOOP WALKS THE PATH SET FOR THE PIECE
-			for(unsigned i = 1; i <abs(bishopR - finishR); i++){
-				std::cout << "testing testing" << std::endl;
+                        for(unsigned i = 1; i <abs(bishopR - finishR); i++){
+                                std::cout << "testing testing" << std::endl;
 //CHECKING IF THERE IS ANY PIECES IN THE WAY
-				if(b[bishopR + increment_r * i][bishopC + increment_c *i].isEmpty() != false ){
-					return false;
-				}
-			}
-		}
-		else{
-			return false;
-		}
-		
-		if(flag == false){return true;}
+                                if(throwawayBoard[bishopR + increment_r * i][bishopC + increment_c *i].isEmpty() != false ){
+                                        return false;
+                                }
+                        }
+                }
+                else{
+                        return false;
+                }
 
-  */ return true;    }
+                if(flag == false){return true;}
+
+   return true;    }
 
 };
 
