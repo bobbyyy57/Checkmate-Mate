f80c9ce4dd1f20e6aea4ea1c30188bba7cf0e696
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2021-03-20 16:46:10 -0700

    Fixed bugs in each Piece Class; still adjusting to VSCode

--- Empty.h
+++ Empty.h
@@ -7,15 +7,15 @@
 
 using namespace std;
 
-
-class Empty : public Piece {
-        public:
-                Empty() {};
-                virtual string getType() { return "Empty"; }
-                virtual bool isValid(Position* start, Position* end, Board b) {
-			return true;
-		}
-
+class Empty : public Piece
+{
+public:
+        Empty(){};
+        virtual string getType() { return "Empty"; }
+        virtual bool isValid(Position *start, Position *end, Board b)
+        {
+                return true;
+        }
 };
 
 #endif
